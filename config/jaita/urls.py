2abb7f575ebd3ca847e96aab4f1ae601c26f81bc
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2024-04-04 22:36:25 +0300

    implement Logout

--- config/jaita/urls.py
+++ config/jaita/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView, LoginView, HomeView
+from .views import SignupView, LoginView, HomeView, LogoutView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', HomeView.as_view(), name='home'),
 ]
